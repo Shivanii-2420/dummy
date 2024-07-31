740d241b85b6817413bb039812e4fd85319d822e
Shivanii-2420 <shivanii.422008@gmail.com>
2024-07-31 14:52:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+#Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")
+
+
+#---HEADER SECTION---
+st.subheader("Hi, I am shwey :wave:")
+st.title("A student from AIML")
+st.write("bruh, its so boring")
+st.write("[Learn More>] (https://pythonandvba.com)")
